b9cc28aabcc337b562b3be79132f0a7f9d1e9e8e
Notheman <109024890+hotmanX813@users.noreply.github.com>
2023-09-07 23:16:23 +0100

    Add files via upload

---
+++ snippets/engine.js
@@ -0,0 +1,7 @@
+function giveInfo() {
+    let senderName = document.getElementById("sendName").value;
+}
+function randomNumber() {
+    var number = Math.floor(random() * 10) ;
+    console.log(number);
+}
